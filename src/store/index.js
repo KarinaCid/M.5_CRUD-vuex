b76e6dc4c508ebfadebe6ccbd64efb85300076bd
Karina Cid <memento_mori.06@hotmail.com>
2020-10-30 23:21:44 -0300

    Project CRUD

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    usersList: []
   },
   mutations: {
+    SET_USERS_LIST(state, users) {
+      state.usersList = users
+    }
   },
   actions: {
+    getUsersList({commit}) {
+      axios.get(`https://us-central1-ottoklauss-5927c.cloudfunctions.net/api/users`)
+      .then(resp => commit('SET_USERS_LIST', resp.data)) 
+    },
+    createUser({dispatch}, user) {
+      axios.post(`https://us-central1-ottoklauss-5927c.cloudfunctions.net/api/users`, user)
+      .then(resp => dispatch('getUsersList', resp.data)) 
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <user-form></user-form>
+    <h2>Listado</h2>
+    <ul>
+      <li v-for="user in usersList" :key="user.id">
+        {{ user.data.name }} | {{ user.data.email }}
+      </li>
+      <a href="#">Eliminar</a>
+      |
+      <a href="#">Editar</a>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import UserForm from "@/components/UserForm";
+import { mapState, mapActions } from "vuex";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    UserForm,
+  },
+  computed: {
+    ...mapState(["usersList"]),
+  },
+  methods: {
+    ...mapActions(["getUsersList"]),
+  },
+  created() {
+    this.getUsersList();
+  },
+};
 </script>
